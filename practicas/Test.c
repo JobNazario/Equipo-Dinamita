53a908a0efbc6a25689ba63b74fe44e2561b8447
Linda <lincitlali@gmail.com>
2020-09-24 19:24:28 -0500

    Ejemplo de Branch

--- practicas/Test.c
+++ practicas/Test.c
@@ -1,18 +1,9 @@
 #include "stidio.h" 
 
-  
-
-  
+ 
 
 void main ( void ) 
-
 { 
-
-    printf("I am PM"); 
-	printf("I am JNC");
- 	printf("I am LCRA");
-	printf("hola que tal\n"); 
-=======
->>>>>>> LRA
+    printf("Asesorias a Citlali");
+    pritnf("De 7 a ...");
 } 
-
