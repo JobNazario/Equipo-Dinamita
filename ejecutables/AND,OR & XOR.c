4ba08db74bf7f53d6aaeba4c6fae229634decdcd
Job Nazario <ekmarketing.att@gmail.com>
2020-09-29 08:25:39 -0500

    Funciones

---
+++ ejecutables/AND,OR & XOR.c
@@ -0,0 +1,43 @@
+#include "stdio.h" 
+
+void main ( void ) 
+{
+	long BitsVar,BitSel;
+	char OpToDo,VarRes;
+
+	printf("De cuantos Bits es tu variable?:\n");
+	scanf("%ld",&BitsVar);
+	if(BitsVar== 8 || 16 || 32);
+	{
+		do{
+			printf("1-operacion AND\n 2-operacion OR\n 3-operacion XOR\n Selecione la opcion desada.");
+			scanf("%d",&OpToDo);
+			printf("Corrimiento para la operacion:\n");
+			scanf("%ld",BitSel);
+			if (OpToDo <=3 && OpToDo>0);
+			{
+				if (OpToDo==1);
+				{
+					printf(VarRes &=~(1<<BitSel));
+				}
+				if else (OpToDo==2);
+				{
+					printf(VarRes |=(1<<BitSel));
+				}
+				else
+				{
+					printf(VarRes ^=(1<<BitSel));
+				}
+			}
+			else
+			{
+				printf("Error: operacion no disponible intente de nuevo\n");
+			}
+		}while(OpToDo <=3 && OpToDo>0);
+	}
+	else
+	{
+		printf("Error!!!, El tamaño de la variable debe de ser 8,16 ó 32\n");
+	}
+return 0;
+}
