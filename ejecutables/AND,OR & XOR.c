6a2018fd8cbf7ec8372d288999a2108979428440
Linda <lincitlali@gmail.com>
2020-09-29 08:47:46 -0500

    correction2

--- ejecutables/AND,OR & XOR.c
+++ ejecutables/AND,OR & XOR.c
@@ -7,21 +7,21 @@ void main(void)
 
 	printf("De cuantos Bits es tu variable?:\n");
 	scanf("%ld",&BitsVar);
-	if(BitsVar == 8 || 16 || 32);
+	if(BitsVar == 8 || 16 || 32)
 	{
 		do{
 			printf("1-operacion AND\n 2-operacion OR\n 3-operacion XOR\n Selecione la opcion desada.");
 			scanf("%d",&OpToDo);
 			printf("Corrimiento para la operacion:\n");
 			scanf("%ld",BitSel);
-			if (OpToDo <=3 && OpToDo>0);
+			if (OpToDo <=3 && OpToDo>0)
 			{
-				if (OpToDo==1);
+				if (OpToDo==1)
 				{
 					VarRes&=~(1<<BitSel);
 					printf("%ld",VarRes);
 				}
-				if else (OpToDo==2);
+				else if (OpToDo==2)
 				{
 					VarRes|=(1<<BitSel);
 					printf("%ld",VarRes);
