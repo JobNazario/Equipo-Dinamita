46d34b4ec8c762c79c912a075bd9cdfe370a2e3c
Job Nazario <ekmarketing.att@gmail.com>
2020-10-10 00:09:20 -0500

    CapsOff2.0

--- practicas/Lab01/GENFUN.C
+++ practicas/Lab01/GENFUN.C
@@ -1,20 +1,22 @@
 #include "stdio.h"
 #include "GENFUN.h"
 
-void main(void)
+int main(void)
 {
-	uint8 au8BuferTest[12]="La practica esta terminada"
-	printf("Buffer befor funtion %s\n",au8BuferTest );
-	GENFUN_vCapsOff(au8BuferTest[0],12);
-	printf("Buffer befor funtion %s\n",au8BuferTest );
+	uint8 au8BuferTest[12] = "Hello world";
+	printf("Buffer befor funtion %s\n", au8BuferTest);
+	GENFUN_vCapsOff(&au8BuferTest[0],12);
+	printf("Buffer afther funtion %s\n", au8BuferTest);
+	GENFUN_vCapsOn(&au8BuferTest[0],12);
+	printf("Buffer befor funtion %s\n", au8BuferTest);
 }
 
 void GENFUN_vCapsOff (uint8 *pu8Src, uint8 u8SizeOfList) 
 {
 	while(u8SizeOfList !=0)
 	{
-		if(	*pu8Src => ASCCI_LOW_THRESHOLD_CAPSON_TO_OFF &&
-			*pu8Src <=ASCCI_HIGH_THRESHOLD_CAPSON_TO_OFF )
+		if(	*pu8Src >= ASCCI_LOW_THRESHOLD_CAPSON_TO_OFF &&
+			*pu8Src <= ASCCI_HIGH_THRESHOLD_CAPSON_TO_OFF )
 		{
 			*pu8Src += ASCCI_COVERTION_FACTOR;
 		}
@@ -26,3 +28,22 @@ void GENFUN_vCapsOff (uint8 *pu8Src, uint8 u8SizeOfList)
 		u8SizeOfList --;
 	}
 }
+
+void GENFUN_vCapsOn (uint8 *pu8Src, uint8 u8SizeOfList) 
+{
+	while(u8SizeOfList !=0)
+	{
+		if(	*pu8Src >= ASCCI_LOW_THRESHOLD_CAPSON_TO_OFF &&
+			*pu8Src <= ASCCI_HIGH_THRESHOLD_CAPSON_TO_OFF )
+		{
+			*pu8Src -= ASCCI_COVERTION_FACTOR;
+		}
+		else
+		{
+			/*Nothing to do*/
+		}
+		pu8Src++;
+		u8SizeOfList --;
+	}
+}
+
