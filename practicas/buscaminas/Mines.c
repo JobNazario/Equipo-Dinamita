4754d4b90cddde9d54027686ccc286875d40be34
Job Nazario <ekmarketing.att@gmail.com>
2020-11-06 23:13:08 -0600

    2

--- practicas/buscaminas/Mines.c
+++ practicas/buscaminas/Mines.c
@@ -1,238 +1,124 @@
-#include <assert.h>  // assert
-#include <ctype.h>   // toupper
-#include <stdio.h>   // printf, scanf
-#include <stdlib.h>  // rand
-#include <unistd.h>  // getpid
+/**************************
 
-// Cosas que no deberías modificar si no sabes lo que haces
-#define ERROR_MINA_ENCONTRADA 1
-#define ERROR_ESPACIO_YA_DESCUBIERTO 2
-#define ERROR_NINGUNO 3
+buscaminas
+***************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <Mines.h>
 
-// Cosas que puedes modificar ;)
-#define COLUMNAS 5
-#define FILAS 5
-#define ESPACIO_SIN_DESCUBRIR '.'
-#define ESPACIO_DESCUBIERTO ' '
-#define MINA '*'
-#define CANTIDAD_MINAS \
-  5  // ¿cuántas minas colocar en el tablero de manera aleatoria? va a fallar si
-     // hay menos espacio que el número de minas
-#define DEBUG 0  // Si lo pones en 1, se van a desocultar las minas
-
-// Devuelve el número de minas que hay cercanas en determinada coordenada
-int obtenerMinasCercanas(int fila, int columna, char tablero[FILAS][COLUMNAS]) {
-  int conteo = 0, filaInicio, filaFin, columnaInicio, columnaFin;
-  if (fila <= 0) {
-    filaInicio = 0;
-  } else {
-    filaInicio = fila - 1;
-  }
-
-  if (fila + 1 >= FILAS) {
-    filaFin = FILAS - 1;
-  } else {
-    filaFin = fila + 1;
-  }
-
-  if (columna <= 0) {
-    columnaInicio = 0;
-  } else {
-    columnaInicio = columna - 1;
-  }
-  if (columna + 1 >= COLUMNAS) {
-    columnaFin = COLUMNAS - 1;
-  } else {
-    columnaFin = columna + 1;
-  }
-  int m;
-  for (m = filaInicio; m <= filaFin; m++) {
-    int l;
-    for (l = columnaInicio; l <= columnaFin; l++) {
-      if (tablero[m][l] == MINA) {
-        conteo++;
-      }
+uint32 main()
+{
+    uint8 BM[15][15]={{0}};
+    uint32 renglon,columna;
+    uint32 nv=0;
+    srand(time(NULL));
+    nv=menu();
+    if(nv!=0)
+    {
+    minas(BM,nv);
+    do{
+        despliega(BM);
+        printf("en cuál renglón?");
+        scanf("%i",&renglon);
+        printf("En cual columna?");
+        scanf("%i",&columna);
+        
+        if(checaMinas(BM,renglon,columna)==-1)
+           {
+               printf("Booom");
+               break;
+           }
+    }while(1); //TRUE
     }
-  }
-  return conteo;
-}
-
-// Devuelve un número aleatorio entre minimo y maximo, incluyendo a minimo y
-// maximo
-int aleatorioEnRango(int minimo, int maximo) {
-  return minimo + rand() / (RAND_MAX / (maximo - minimo + 1) + 1);
-}
-// Rellena el tablero de espacios sin descubrir
-void iniciarTablero(char tablero[FILAS][COLUMNAS]) {
-  int l;
-  for (l = 0; l < FILAS; l++) {
-    int m;
-    for (m = 0; m < COLUMNAS; m++) {
-      tablero[l][m] = ESPACIO_SIN_DESCUBRIR;
+    else if(nv=0)
+    {
+       printf(" ERROR \n");
     }
-  }
-}
 
-// Coloca una mina en las coordenadas indicadas
-void colocarMina(int fila, int columna, char tablero[FILAS][COLUMNAS]) {
-  tablero[fila][columna] = MINA;
+    return 0;
 }
-
-// Coloca minas de manera aleatoria. El número depende de CANTIDAD_MINAS
-void colocarMinasAleatoriamente(char tablero[FILAS][COLUMNAS]) {
-  int l;
-  for (l = 0; l < CANTIDAD_MINAS; l++) {
-    int fila = aleatorioEnRango(0, FILAS - 1);
-    int columna = aleatorioEnRango(0, COLUMNAS - 1);
-    colocarMina(fila, columna, tablero);
-  }
-}
-
-void imprimirSeparadorEncabezado() {
-  int m;
-  for (m = 0; m <= COLUMNAS; m++) {
-    printf("----");
-    if (m + 2 == COLUMNAS) {
-      printf("-");
+uint32 menu(void)
+{ 
+    uint32 nv =0;
+    uint32 nivel=0;
+    printf("nivel a jugar:   \n");
+    scanf("%i",&nv);
+    if(nv==1)
+    {
+        nivel=10;
+        
     }
-  }
-  printf("\n");
+    else if (nv==2)
+    {
+        nivel=20;
+        
+    }  
+    else if (nv==3)
+    {
+        nivel=30;
+        
+    }
+    else if (nv>3)
+    {
+        printf(" ERROR \n");
+        nivel=0;
+    }
+    return nivel;
+    
+}
+uint32 checaMinas(uint8 bm[15][15], uint32 j, uint32 i)
+{
+    uint32 contar =0;
+    if(bm[j][i]=='*')
+     return-1;
+    if(bm[j-1][i]=='*')
+        contar++;
+    if(bm[j-1][i+1]=='*')
+        contar++;
+    if(bm[j][i-1]=='*')
+        contar++;
+    if(bm[j][i+1]=='*')
+        contar++;
+    if(bm[j+1][i-1]=='*')
+        contar++;
+    if(bm[j+1][i]=='*')
+        contar++;
+    if(bm[j+1][i+1]=='*')
+        contar++;
+    bm[j][i]=contar;
+    
+}
+void despliega(uint8 arr[15][15]) // interfas 
+{
+     uint32 i, j;
+    for(j=0;j<15;j++)
+    {
+        for(i=0;i<15;i++)
+        {
+            if(arr[j][i]=='*')
+                printf("0");//minas
+            else 
+                printf("%i",arr[j][i]);
+                
+        }
+        puts("");
+    }
+}
+void minas(uint8 BM[15][15], uint32 nivel)  // pone minas Booom
+{
+    uint8 minitas=0, x, y; 
+    
+    do{
+      x=rand()%8+1; //protege los margenes de error 
+      y=rand()%8+1;
+    
+    if (BM[x][y]==0)  // cambia por el do
+        {
+            BM[x][y]='*';
+            minitas++;
+        }
+    
+    }while(minitas<nivel); 
 }
 
-void imprimirSeparadorFilas() {
-  int m;
-  for (m = 0; m <= COLUMNAS; m++) {
-    printf("+---");
-    if (m == COLUMNAS) {
-      printf("+");
-    }
-  }
-  printf("\n");
-}
-
-void imprimirEncabezado() {
-  imprimirSeparadorEncabezado();
-  printf("|   ");
-  int l;
-  for (l = 0; l < COLUMNAS; l++) {
-    printf("| %d ", l + 1);
-    if (l + 1 == COLUMNAS) {
-      printf("|");
-    }
-  }
-  printf("\n");
-}
-// Convierte un int a un char. Por ejemplo 0 a '0'
-char enteroACaracter(int numero) {
-  return numero + '0';
-}
-
-void imprimirTablero(char tablero[FILAS][COLUMNAS], int deberiaMostrarMinas) {
-  imprimirEncabezado();
-  imprimirSeparadorEncabezado();
-  char letra = 'A';
-  int l;
-  for (l = 0; l < FILAS; l++) {
-    int m;
-    // Imprimir la letra de la fila
-    printf("| %c ", letra);
-    letra++;
-    for (m = 0; m < COLUMNAS; m++) {
-      // No le vamos a mostrar al usuario si hay una mina...
-      char verdaderaLetra = ESPACIO_SIN_DESCUBRIR;
-      char letraActual = tablero[l][m];
-      if (letraActual == MINA) {
-        verdaderaLetra = ESPACIO_SIN_DESCUBRIR;
-      } else if (letraActual == ESPACIO_DESCUBIERTO) {
-        // Si ya lo abrió, entonces mostramos las minas cercanas
-        int minasCercanas = obtenerMinasCercanas(l, m, tablero);
-        verdaderaLetra = enteroACaracter(minasCercanas);
-      }
-      // Si DEBUG está en 1, o debería mostrar las minas (porque perdió o ganó)
-      // mostramos la mina original
-      if (letraActual == MINA && (DEBUG || deberiaMostrarMinas)) {
-        verdaderaLetra = MINA;
-      }
-      printf("| %c ", verdaderaLetra);
-      if (m + 1 == COLUMNAS) {
-        printf("|");
-      }
-    }
-    printf("\n");
-    imprimirSeparadorFilas();
-  }
-}
-
-// Recibe la fila, columna y tablero. La fila y columna deben ser tal y como las
-// proporciona el usuario. Es decir, la columna debe comenzar en 1 (no en cero
-// como si fuera un índice) y la fila debe ser una letra
-int abrirCasilla(char filaLetra, int columna, char tablero[FILAS][COLUMNAS]) {
-  // Convertir a mayúscula
-  filaLetra = toupper(filaLetra);
-  // Restamos 1 porque usamos la columna como índice
-  columna--;
-  // Convertimos la letra a índice
-  int fila = filaLetra - 'A';
-  assert(columna < COLUMNAS && columna >= 0);
-  assert(fila < FILAS && fila >= 0);
-  if (tablero[fila][columna] == MINA) {
-    return ERROR_MINA_ENCONTRADA;
-  }
-  if (tablero[fila][columna] == ESPACIO_DESCUBIERTO) {
-    return ERROR_ESPACIO_YA_DESCUBIERTO;
-  }
-  // Si no hay error, colocamos el espacio descubierto
-  tablero[fila][columna] = ESPACIO_DESCUBIERTO;
-  return ERROR_NINGUNO;
-}
-
-// Para saber si el usuario ganó
-int noHayCasillasSinAbrir(char tablero[FILAS][COLUMNAS]) {
-  int l;
-  for (l = 0; l < FILAS; l++) {
-    int m;
-    for (m = 0; m < COLUMNAS; m++) {
-      char actual = tablero[l][m];
-      if (actual == ESPACIO_SIN_DESCUBRIR) {
-        return 0;
-      }
-    }
-  }
-  return 1;
-}
-
-int main() {
-  printf("** BUSCAMINAS **\nBy Parzibyte\n");
-  char tablero[FILAS][COLUMNAS];
-  int deberiaMostrarMinas = 0;
-  // Alimentar rand
-  srand(getpid());
-  iniciarTablero(tablero);
-  colocarMinasAleatoriamente(tablero);
-  // Ciclo infinito. Se rompe si gana o pierde, y eso se define con
-  // "deberiaMostrarMinas"
-  while (1) {
-    imprimirTablero(tablero, deberiaMostrarMinas);
-    if (deberiaMostrarMinas) {
-      break;
-    }
-    int columna;
-    char fila;
-    printf("Ingresa la fila: ");
-    scanf(" %c", &fila);
-    printf("Ingresa la columna: ");
-    scanf("%d", &columna);
-    int status = abrirCasilla(fila, columna, tablero);
-    if (noHayCasillasSinAbrir(tablero)) {
-      printf("Has ganado\n");
-      deberiaMostrarMinas = 1;
-    } else if (status == ERROR_ESPACIO_YA_DESCUBIERTO) {
-      printf("Ya has abierto esta casilla\n");
-    } else if (status == ERROR_MINA_ENCONTRADA) {
-      printf("Has perdido\n");
-      deberiaMostrarMinas = 1;
-    }
-  }
-  return 0;
-}
 
---
+++ practicas/buscaminas/Mines.h
@@ -0,0 +1,9 @@
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long long uint64;
+
+void minas(uint8 BM[15][15],uint32 nivel);
+void despliega(uint8 arr[15][15]);
+uint32 checaMinas(uint8 bm[15][15], uint32 j, uint32 i);  //-1
+uint32 menu(void);
